b8f906444e6ab0febcbbb582399cf0deac07361a
Guilherme Santos Maciel <guilhermemaciel@plusoft.com>
2022-05-13 11:14:36 -0300

    Beecrowd Problems

---
+++ 1020/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias = Int32.Parse(Console.ReadLine());
+
+            int anos = (int)dias / 365;
+            dias = dias - (anos * 365);
+
+            int meses = (int)dias / 30;
+            dias = dias - (meses * 30);
+
+            Console.WriteLine($"{anos} ano(s)");
+            Console.WriteLine($"{meses} mes(es)");
+            Console.WriteLine($"{dias} dia(s)");
+        }
+    }
+}
---
+++ 1036/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1036
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha = Console.ReadLine();
+            String[] v = linha.Split(' ');
+
+            double a = double.Parse(v[0]);
+            double b = double.Parse(v[1]);
+            double c = double.Parse(v[2]);
+
+            if (a != 0)
+            {
+                double delta = (b * b) - 4 * a * c;
+                if (delta >= 0)
+                {
+                    delta = Math.Sqrt(delta);
+                    double x1 = (-b + delta) / (2 * a);
+                    double x2 = (-b - delta) / (2 * a);
+
+                    Console.WriteLine($"R1 = {x1:F5}");
+                    Console.WriteLine($"R2 = {x2:F5}");
+                }
+                else
+                {
+                    Console.WriteLine("Impossivel calcular");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+
+        }
+    }
+}
